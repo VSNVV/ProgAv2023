f3f4661eeffbf1fa56c2c2d7db79657058620adc
viti <victor.sanavia@edu.uah.es>
2023-03-28 13:51:42 +0200

    Avanzadas las hormigas soldada
    Falta por codificar el run y el evento de insecto invasor

--- PECL/src/main/java/concurrencia/ZonaDescanso.java
+++ PECL/src/main/java/concurrencia/ZonaDescanso.java
@@ -1,4 +1,58 @@
 package concurrencia;
 
+import javax.swing.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ZonaDescanso {
+    //Atributos de la clase ZonaDescanso
+    private Log log;
+    private Lock entradaSalida = new ReentrantLock();
+    private ListaThreads listaHormigasDescansando;
+
+    //Métodos de la clase ZonaDescanso
+
+    //Método constructor
+    public ZonaDescanso(Log log, JTextField jTextFieldHormigasDescansando){
+        this.log = log;
+        this.listaHormigasDescansando = new ListaThreads(jTextFieldHormigasDescansando);
+    }
+
+    //Método para entrar a la zona de descanso
+    public void entraZonaDescanso(Hormiga hormiga){
+        entradaSalida.lock();
+        try{
+            //Para entrar, metemos el identificador de la hormiga en el JTextField correspondiente
+            getListaHormigasDescansando().meterHormiga(hormiga);
+            //Una vez dentro, escribimos el evento en el log
+            getLog().escribirEnLog("[Zona Descanso] --> La hormiga " + hormiga.getIdentificador() + " ha entrado a la zona de descanso");
+
+        }finally{
+            entradaSalida.unlock();
+        }
+    }
+
+    //Método para salir de la zona de descanso
+    public void saleZonaDescanso(Hormiga hormiga){
+        entradaSalida.lock();
+        try{
+            //Para salir, tenemos que quitar el identificador de la hormiga del JTextField correspondiente
+            getListaHormigasDescansando().sacarHormiga(hormiga);
+            //Una vez fuera, escribimos el evento en el log
+            getLog().escribirEnLog("[Zona Descanso] --> La hormiga " + hormiga.getIdentificador() + " ha salido de la zona de descanso");
+
+        }finally{
+            entradaSalida.unlock();
+        }
+    }
+
+    //Métodos get y set
+
+    public Log getLog() {
+        return log;
+    }
+
+    public ListaThreads getListaHormigasDescansando() {
+        return listaHormigasDescansando;
+    }
 }
--- PECL/src/main/java/concurrencia/ZonaInstruccion.java
+++ PECL/src/main/java/concurrencia/ZonaInstruccion.java
@@ -1,4 +1,68 @@
 package concurrencia;
 
+import javax.swing.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ZonaInstruccion {
+    //Atributos de la clase ZonaInstruccion
+    private Log log;
+    private Lock entradaSalida = new ReentrantLock();
+    private ListaThreads listaHormigasHaciendoInstruccion;
+
+    //Métodos de la clase ZonaInstruccion
+
+    //Método constructor
+    public ZonaInstruccion(Log log, JTextField jTextFieldHormigasHaciendoInstruccion){
+        this.listaHormigasHaciendoInstruccion = new ListaThreads(jTextFieldHormigasHaciendoInstruccion);
+    }
+
+    //Método para entrar a la zona de instruccion
+    public void entraZonaInstruccion(Hormiga hormiga){
+        entradaSalida.lock();
+        try{
+            //Cuando entra una hormiga, mete su ID al JTextField correspondiente mediante el ListaThreads
+            getListaHormigasHaciendoInstruccion().meterHormiga(hormiga);
+            //Una vez metida, escribe el evento en el log
+            getLog().escribirEnLog("[Zona Instruccion] --> La hormiga " + hormiga.getIdentificador() + " ha entrado a la zona de instruccion");
+
+        }finally{
+            entradaSalida.unlock();
+        }
+    }
+    //Método para salir de la zona de instruccion
+    public void saleZonaInstruccion(Hormiga hormiga){
+        entradaSalida.lock();
+        try{
+            //Para salir, en primer lugar tenemos que quitar su ID del JTextField
+            getListaHormigasHaciendoInstruccion().sacarHormiga(hormiga);
+            //Una vez fuera, escribe el mensaje en el log
+            getLog().escribirEnLog("[Zona Instruccion] --> La hormiga " + hormiga.getIdentificador() + " ha salido de la zona de instruccion");
+
+        }finally{
+            entradaSalida.unlock();
+        }
+    }
+
+    //Método para hacer instruccion
+    public void realizaInstruccion(Hormiga hormiga){
+        //Hacen instrucción entre 2 y 8 segundos
+        getLog().escribirEnLog("[Zona Instruccion] --> La hormiga " + hormiga.getIdentificador() + " esta haciendo instruccion");
+        try{
+            Thread.sleep((int) ((Math.random() + 1) * 4000 + 4000));
+        }catch (InterruptedException ignored){}
+    }
+    //Métodos get y set
+
+    public Log getLog() {
+        return log;
+    }
+
+    public Lock getEntradaSalida() {
+        return entradaSalida;
+    }
+
+    public ListaThreads getListaHormigasHaciendoInstruccion() {
+        return listaHormigasHaciendoInstruccion;
+    }
 }
