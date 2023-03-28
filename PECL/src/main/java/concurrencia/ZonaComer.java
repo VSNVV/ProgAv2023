b539c963f65b89e9cb255eeae9dd76b9eee23ec4
viti <victor.sanavia@edu.uah.es>
2023-03-28 18:49:38 +0200

    Implementado el mecanismo de comunicacion entre hilos a la hora de comer
    Ahora si no hay comida, la hormiga esperará a que haya una unidad para cogerla

--- PECL/src/main/java/concurrencia/ZonaComer.java
+++ PECL/src/main/java/concurrencia/ZonaComer.java
@@ -1,6 +1,7 @@
 package concurrencia;
 
 import javax.swing.*;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,7 +9,9 @@ public class ZonaComer {
     //Atrubutos de la clase ZonaComer
     private Log log;
     private Lock entradaSalida = new ReentrantLock(), elementoComida = new ReentrantLock();
+    private Condition esperaAlimento = elementoComida.newCondition();
     private int numElementosComida = 0;
+    private boolean hormigaEsperandoAlimento = false;
     private ListaThreads unidadesElementosComida, listaHormigasLlevandoComida, listaHormigasZonaComer;
 
 
@@ -53,8 +56,56 @@ public class ZonaComer {
             Thread.sleep((int) (Math.random() * 1000 + 1000));
             //Una vez transcurrido el timepo, incrementamos el numero de elementos de comida
             setNumElementosComida(getNumElementosComida() + 1);
+            //Imprimimos el numero en el JTextField
+            getUnidadesElementosComida().insertarNumero(getNumElementosComida());
+            //Antes de irnos, miraremos si hay alguna hormiga esperando comida
+            if(isHormigaEsperandoAlimento()){
+                //Se verifica que hay una hormiga esperando alimento, por tanto tenemos que despertarla para que coma
+                esperaAlimento.signal();
+                //Como no hay hormigas esperando, pondremos el booleano a false
+                setHormigaEsperandoAlimento(false);
+            }
 
         } catch (InterruptedException e) {}
+        finally{
+            elementoComida.unlock();
+        }
+    }
+
+    //Método para que una hormiga coma
+    public void come(Hormiga hormiga){
+        try{
+            cogeElementoComida(hormiga);
+            getLog().escribirEnLog("[Zona Comer] --> La hormiga " + hormiga.getIdentificador() + " esta comiendo");
+            //Dependiendo del tipo de hormiga, tarda más o menos en comer
+            if((hormiga.getTipo() == "Obrera") || (hormiga.getTipo() == "Soldada")){
+                //Las hormigas obreras y soldadas tardan en comer 3 segundos
+                Thread.sleep(3000);
+            }
+            else if(hormiga.getTipo() == "Cria"){
+                //Una hormiga cria tarda en comer entre 3 y 5 segundos
+                Thread.sleep((int) (((Math.random() + 1) * 3000 ) + 2000));
+            }
+
+        }catch(InterruptedException ignored){}
+
+    }
+
+    //Método auxiliar al método comer
+    private void cogeElementoComida(Hormiga hormiga){
+        elementoComida.lock();
+        try{
+            //En primer lugar tenemos que comprobar si hay un elemento de comida disponible
+            if(getNumElementosComida() == 0){
+                //Se verifica que no hay elementos de comida disponibles, por tanto tendrán que esperar a que haya alguno
+                //Indicaremos que estamos esperando
+                setHormigaEsperandoAlimento(true);
+                //Una vez indicado, esperaremos a que un hilo nos despierte
+                esperaAlimento.await();
+            }
+            //En el caso de que no haya que esperar, simplemente cogemos el alimento
+            setNumElementosComida(getNumElementosComida() - 1);
+        }catch(InterruptedException ignored) {}
         finally{
             elementoComida.unlock();
         }
@@ -69,5 +120,25 @@ public class ZonaComer {
     }
     public void setNumElementosComida(int numElementosComida){
         this.numElementosComida = numElementosComida;
+    }
+
+    public ListaThreads getListaHormigasLlevandoComida() {
+        return listaHormigasLlevandoComida;
+    }
+
+    public ListaThreads getListaHormigasZonaComer() {
+        return listaHormigasZonaComer;
+    }
+
+    public ListaThreads getUnidadesElementosComida(){
+        return unidadesElementosComida;
+    }
+
+    public boolean isHormigaEsperandoAlimento() {
+        return hormigaEsperandoAlimento;
+    }
+
+    public void setHormigaEsperandoAlimento(boolean hormigaEsperandoAlimento) {
+        this.hormigaEsperandoAlimento = hormigaEsperandoAlimento;
     }
 }
