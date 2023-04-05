c6ef177e8a9ed4de29ba7f3e90dbe64a5c711556
viti <victor.sanavia@edu.uah.es>
2023-04-05 14:11:18 +0200

    Mejoras en GestorInterfaz
    Los métodos ahora están mejor redactados y más sencillos de entender
    
    Se han añadido dos métodos privados al final que sirven a todas las funciones definidas en la InterfazColonia

--- PECL/src/main/java/distribuida/GestorInterfaz.java
+++ PECL/src/main/java/distribuida/GestorInterfaz.java
@@ -2,14 +2,13 @@ package distribuida;
 
 import concurrencia.*;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 public class GestorInterfaz extends UnicastRemoteObject implements InterfazColonia {
     //Métodos de la clase colonia
-    private Colonia colonia;
+    private final Colonia colonia;
 
     //Método constructor
     public GestorInterfaz(Colonia colonia) throws RemoteException {
@@ -18,78 +17,42 @@ public class GestorInterfaz extends UnicastRemoteObject implements InterfazColon
 
     //Método que devuelve el numero de hormigas obreras en el exterior de la colonia
     public String numHomrigasObrerasExteriorColonia() throws RemoteException {
-        String resultado;
-        int num;
-        ArrayList<Hormiga> lista = getColonia().getListaHormigasLlevandoComida().getListaHormigas();
-        num = lista.size();
-        resultado = String.valueOf(num);
-        return resultado;
+        ArrayList<Hormiga> lista = getColonia().getListaHormigasBuscandoComida().getListaHormigas();
+        return cuentaHormigasArraylist(lista);
     }
 
     //Método que devuelve el numero de hormigas obreras dentro de la colonia
     public String numHomrigasObrerasInteriorColonia() throws RemoteException {
-        String resultado;
-        int num = 0;
         ArrayList<Hormiga> lista = getColonia().getListaHormigas();
-        for(int i = 0; i < lista.size(); i++){
-            Hormiga hormigaActual = lista.get(i);
-            String tipo = hormigaActual.getTipo();
-            if(tipo.equals("Obrera")){
-                num = num + 1;
-            }
-        }
-        resultado = String.valueOf(num);
-        return resultado;
+        return cuentaHormigasTipoArraylist(lista, "Obrera");
     }
 
     //Método que devuelve el numero de hormigas soldado haciendo instruccion
     public String numHormigasSoldadoInstruccion() throws RemoteException {
-        String resultado;
-        int num;
         ZonaInstruccion zonaInstruccion = getColonia().getZonaInstruccion();
         ArrayList<Hormiga> lista = zonaInstruccion.getListaHormigasHaciendoInstruccion().getListaHormigas();
-        num = lista.size();
-        resultado = String.valueOf(num);
-        return resultado;
+        return cuentaHormigasArraylist(lista);
     }
 
     //Método que devuelve el numero de hormigas luchando en la invasion
     public String numHormigasSoldadoInvasion() throws RemoteException {
-        String resultado;
-        int num;
         Invasion invasion = getColonia().getInvasion();
         ArrayList<Hormiga> lista = invasion.getListaHormigasInvasion().getListaHormigas();
-        num = lista.size();
-        resultado = String.valueOf(num);
-        return resultado;
+        return cuentaHormigasArraylist(lista);
     }
 
     //Método que devuelve el número de hormigas cría en la zona para comer
     public String numHormigasCriaZonaComer() throws RemoteException {
-        String resultado;
-        int num = 0;
         ZonaComer zonaComer = getColonia().getZonaComer();
         ArrayList<Hormiga> lista = zonaComer.getListaHormigasZonaComer().getListaHormigas();
-        for(int i = 0; i < lista.size(); i++){
-            Hormiga hormigaActual = lista.get(i);
-            String tipo = hormigaActual.getTipo();
-            if(tipo.equals("Cria")){
-                num = num + 1;
-            }
-        }
-        resultado = String.valueOf(num);
-        return resultado;
+        return cuentaHormigasTipoArraylist(lista, "Cria");
     }
 
     //Método que devuelve el número de hormigas cría en el refugio
     public String numHormigasCriaRefugio() throws RemoteException {
-        String resultado;
-        int num;
         Refugio refugio = getColonia().getRefugio();
         ArrayList<Hormiga> lista = refugio.getListaHormigasRefugio().getListaHormigas();
-        num = lista.size();
-        resultado = String.valueOf(num);
-        return resultado;
+        return cuentaHormigasArraylist(lista);
     }
 
     //Método que genera una invasión
@@ -100,5 +63,23 @@ public class GestorInterfaz extends UnicastRemoteObject implements InterfazColon
     //Método get de la colonia
     public Colonia getColonia(){
         return this.colonia;
+    }
+
+    private String cuentaHormigasArraylist(ArrayList<Hormiga> lista){
+        String resultado;
+        int num;
+        num = lista.size();
+        resultado = String.valueOf(num);
+        return resultado;
+    }
+
+    private String cuentaHormigasTipoArraylist(ArrayList<Hormiga> lista, String tipo){
+        int num = 0;
+        for (Hormiga hormigaActual : lista) {
+            if (hormigaActual.getTipo().equals(tipo)) {
+                num = num + 1;
+            }
+        }
+        return String.valueOf(num);
     }
 }
