890011fd8ce0a8d26515b298b731aa94260d53cf
viti <victor.sanavia@edu.uah.es>
2023-04-05 14:05:27 +0200

    Actualizado ListaThreads
    Se ha añadido final a los atributos que lo necesitaban
    
    Se ha mejorad el método de imprimirHormiga

--- PECL/src/main/java/concurrencia/ListaThreads.java
+++ PECL/src/main/java/concurrencia/ListaThreads.java
@@ -6,11 +6,11 @@ import javax.swing.JTextField;
 con métodos para meter y sacar threads en ella. Cada vez que una lista se modifica,
 se imprime su nuevo contenido en el JTextField que toma como parámetro el constructor. */
 public class ListaThreads{
-    private ArrayList<Hormiga> listaHormigas;
-    private JTextField tf;
+    private final ArrayList<Hormiga> listaHormigas;
+    private final JTextField tf;
     
     public ListaThreads(JTextField tf){
-        listaHormigas = new ArrayList<Hormiga>();
+        listaHormigas = new ArrayList<>();
         this.tf = tf;
     }
     
@@ -30,12 +30,11 @@ public class ListaThreads{
     }
 
     private void imprimirHormiga() {
-        String contenido = "";
-        for(int i=0; i<listaHormigas.size(); i++)
-        {
-            contenido = contenido + listaHormigas.get(i).getIdentificador()+" ";
+        StringBuilder contenido = new StringBuilder();
+        for (Hormiga hormigas : listaHormigas) {
+            contenido.append(hormigas.getIdentificador()).append(" ");
         }
-        tf.setText(contenido);
+        tf.setText(contenido.toString());
     }
 
     public ArrayList<Hormiga> getListaHormigas() {
