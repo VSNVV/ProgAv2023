0fb0d454522aa59f06c26d13b199214c7d458bd5
viti <victor.sanavia@edu.uah.es>
2023-04-05 14:09:54 +0200

    Actualizaciones en ZonaDescanso
    Se ha añadido final a los atributos que lo necesitan
    
    Se ha cambiado para una mejor estetica los mensajes del log

--- PECL/src/main/java/concurrencia/ZonaDescanso.java
+++ PECL/src/main/java/concurrencia/ZonaDescanso.java
@@ -6,9 +6,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ZonaDescanso {
     //Atributos de la clase ZonaDescanso
-    private Log log;
-    private Lock entradaSalida = new ReentrantLock();
-    private ListaThreads listaHormigasDescansando;
+    private final Log log;
+    private final Lock entradaSalida = new ReentrantLock();
+    private final ListaThreads listaHormigasDescansando;
 
     //Métodos de la clase ZonaDescanso
 
@@ -19,7 +19,7 @@ public class ZonaDescanso {
     }
 
     //Método para entrar a la zona de descanso
-    public void entra(Hormiga hormiga) throws InterruptedException{
+    public void entra(Hormiga hormiga){
         entradaSalida.lock();
         try{
             //Para entrar, metemos el identificador de la hormiga en el JTextField correspondiente
@@ -39,7 +39,7 @@ public class ZonaDescanso {
             //Para salir, tenemos que quitar el identificador de la hormiga del JTextField correspondiente
             getListaHormigasDescansando().sacarHormiga(hormiga);
             //Una vez fuera, escribimos el evento en el log
-            getLog().escribirEnLog("[Zona Descanso] --> La hormiga " + hormiga.getIdentificador() + " ha salido de la zona de descanso");
+            getLog().escribirEnLog("[ZONA DESCANSO] --> La hormiga " + hormiga.getIdentificador() + " ha salido de la zona de descanso");
 
         }finally{
             entradaSalida.unlock();
@@ -49,12 +49,12 @@ public class ZonaDescanso {
     //Método para que una hormiga haga su descanso
     public void realizaDescanso(Hormiga hormiga) throws InterruptedException{
         //Escribimos el evento en el log
-        getLog().escribirEnLog("[Zona Descanso] --> La hormiga " + hormiga.getIdentificador() + " esta descansando");
-        if (hormiga.getTipo() == "Soldada"){
+        getLog().escribirEnLog("[ZONA DESCANSO] --> La hormiga " + hormiga.getIdentificador() + " esta descansando");
+        if (hormiga.getTipo().equals("Soldada")){
             //Una hormiga soldada descansa un tiempo aleatorio entre 2 y 8 segundos
             Thread.sleep((int) (((Math.random() + 1) * 2000) + (8000 - (2000 * 2))));
         }
-        else if (hormiga.getTipo() == "Obrera"){
+        else if (hormiga.getTipo().equals("Obrera")){
             //Una hormiga obrera realiza un descanso de 1 segundo
             Thread.sleep(1000);
         }
